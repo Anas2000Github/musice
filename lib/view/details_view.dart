598783454b3ba78344f7d2b28edcc8f28a8b58f3
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2023-07-04 13:00:39 +0300

    أضفت صفحة DetailsView
    وما زالت الصفحة في حالة تعديل

--- lib/view/details_view.dart
+++ lib/view/details_view.dart
@@ -1,10 +1,13 @@
 import '../main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../constants.dart';
 import '../controllers/cartcontroller.dart';
+import '../widgets/box_of_size.dart';
 import '../widgets/custom_text.dart';
-import '../widgets/detailsitem.dart';
+// import '../widgets/detailsitem.dart';
+import '../widgets/detailsitem_smallpic.dart';
 // import 'package:flutter_svg/svg.dart';
 
 class DetailsView extends StatelessWidget {
@@ -12,7 +15,7 @@ class DetailsView extends StatelessWidget {
   final String? imgPath;
   final String? productName;
   final String? price;
-  final List<String>? smallPics;
+  List<String>? smallPics;
   RxInt quantity = 1.obs;
   DetailsView({
     Key? key,
@@ -24,91 +27,190 @@ class DetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    smallPics = [imgPath!,imgPath!,imgPath!,imgPath!,];
     return Scaffold(
       appBar: AppBar(
-        title:  CustomText("Details view"),
+        title:  CustomText("Details view",textAlign:TextAlign.center , color: Constants.fontBlackColor),
         elevation: 0.0,
         backgroundColor: Constants.backGroundColor,
         // leading: IconButton(onPressed: (){}, icon: SvgPicture.asset('assets/svg/back.svg')),
       ),
       backgroundColor: Constants.backGroundColor,
-      body: SingleChildScrollView(
-        child: Container(
-          margin: const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
-          padding: const EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
-          height: Get.size.height * .125,
-          width: Get.size.width * .8,
-          decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.1),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
+      body: ListView(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomText("$price"),
+                Image.asset(imgPath!,width: Get.size.width,height: 250,),
               ],
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Constants.fontBlackColor,width: 2)
+            ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //عرض تفاصيل المنتج
-              DetailItemWidget(
-                imgPath: imgPath!,
-                id: id!,
-                  price: price!,
-                productName: productName!,
-                //smallPics: [],
-              ),
-              //Total
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomText(
-                    "Total",
-                  ),
-                  GetBuilder<CartController>(
-                      builder: (controller) {
-                        return CustomText("${controller.totalPrice}");
-                      }
-                  )
-                ],
-              ),
-              //Shipping
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomText(
-                    "Shipping",
-                  ),
-                  GetBuilder<CartController>(
+          Expanded(
+            flex: 4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //عرض تفاصيل المنتج
+                // DetailItemWidget(
+                //   imgPath: imgPath!,
+                //   id: id!,
+                //     price: price!,
+                //   productName: productName!,
+                //   smallPics: [],
+                // ),
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //10.height,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          CustomText("$productName"),
+                          // Text(prcStr),
+                          Row(
+                            children: [
+                              IconButton(
+                                  onPressed: () {
+                                    increment();
+                                  },
+                                  icon: const Icon(CupertinoIcons.plus_circle)),
+                              GetBuilder<CartController>(
+                                  init: Get.put(CartController()),
+                                  builder: (cartController) {
+                                    return CustomText("${quantity.value}");
+                                  }),
+                              IconButton(
+                                  onPressed: () {
+                                    decrement();
+                                  },
+                                  icon: const Icon(CupertinoIcons.minus_circle))
+                            ],
+                          ),
 
-                      builder: (controller) {
-                        return CustomText("${controller.shipping}");
-                      }
-                  )
-                ],
-              ),
-              //Shoe's price
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomText(
-                    "Shoes",
+                        ],
+                      ),
+                      CustomText("Select color"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SmallPic(imagePath: smallPics![0],),
+                          SmallPic(imagePath: smallPics![1],),
+                          SmallPic(imagePath: smallPics![2],),
+                          SmallPic(imagePath: smallPics![3],),
+                        ],
+                      ),
+                      CustomText("Select size"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          BoxOfSize(size: "41",),
+                          BoxOfSize(size: "42",),
+                          BoxOfSize(size: "43",),
+                          BoxOfSize(size: "44",),
+                        ],
+                      ),
+                    ],
                   ),
-                  GetBuilder(
-                      init: CartController(),
-                      builder: (controller) {
-                        return CustomText("${controller.unitPrice}");
-                      }
-                  )
-                ],
-              ),
-            ],
+                ),
+                //Total
+                Expanded(
+                  flex: 4,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomText(
+                        "Total",
+                      ),
+                      GetBuilder<CartController>(
+                          builder: (controller) {
+                            return CustomText("${controller.totalPrice}");
+                          }
+                      )
+                    ],
+                  ),
+                ),
+                //Shipping
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomText(
+                      "Shipping",
+                    ),
+                    GetBuilder<CartController>(
+
+                        builder: (controller) {
+                          return CustomText("${controller.shipping}");
+                        }
+                    )
+                  ],
+                ),
+                //Shoe's price
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomText(
+                      "Shoes",
+                    ),
+                    GetBuilder(
+                        init: CartController(),
+                        builder: (controller) {
+                          return CustomText("${controller.unitPrice}");
+                        }
+                    )
+                  ],
+                ),
+                //زر إنهاء الشراء
+                TextButton(
+                    onPressed: () {
+                      //todo: إنهاء عملية الشراء
+
+                    },
+                    child: Container(
+                      width: 500,
+                      height: 60,
+                      constraints: const BoxConstraints(
+                          maxHeight: 100,
+                          maxWidth: 600,
+                          minHeight: 5,
+                          minWidth: 50),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.blue[900],
+                      ),
+                      child: Center(
+                          child: Image.asset("$imgPath",height: 50,width: 50,)),
+                    ))
+                //نهاية الزر
+              ],
+            ),
           ),
-        ),
+        ],
       ) ,
     );
+  }
+  void increment() {
+    quantity++;
+    setServ.cartController.update();
+    setServ.cartController
+        .incrementTotalPrice(double.parse(price!.substring(1)));
+    Get.put(CartController()).update();
+  }
+
+  void decrement() {
+    if (quantity.value > 1) {
+      quantity--;
+      setServ.cartController.totalPrice =
+          (setServ.cartController.totalPrice.value -
+              double.parse(price!.substring(1)))
+              .obs;
+      setServ.cartController.update();
+    }
   }
 }
